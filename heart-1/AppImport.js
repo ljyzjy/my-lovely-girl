9551117a059317a1fc1943473217c79c70b93cf7
ljyzjy <2805125943@qq.com>
2024-12-04 18:02:42 +0800

    Delete heart-1 directory

--- heart-1/AppImport.js
+++
@@ -1,8 +0,0 @@
-export * as THREE from "three";
-export { Curve } from "three";
-export { SphereGeometry } from "three";
-export { Points } from "three";
-export { Mesh } from "three";
-export { TextGeometry } from "./tools/TextGeometry.js";
-export { FontLoader } from "./tools/FontLoader.js";
-export { OrbitControls } from "./tools/OrbitControls.js";
